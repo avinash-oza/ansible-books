b3ee4d0e72f27596859c34e459a918bd38af60f6
Avi Oza <avioza@gmail.com>
2019-11-10 12:18:53 -0500

    clean up passive check references

--- roles/install-garage-door-service/templates/check_garage.py
+++ roles/install-garage-door-service/templates/check_garage.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-import sys
 import requests
 import argparse
-import warnings
 
-# config is specified by env variable APP_SETTINGS
+
 def check_garage(api_endpoint):
     resp = requests.get(api_endpoint)
     exit_code = 0
@@ -22,24 +20,17 @@ def check_garage(api_endpoint):
         exit_code = 2
     return status_text, exit_code
 
-def submit_check(api_endpoint, passive_check_endpoint, token, hostname):
+def submit_check(api_endpoint):
     status_text, exit_code = check_garage(api_endpoint)
-    submit_cmd = "PROCESS_SERVICE_CHECK_RESULT;{};Garage Status;{};{}".format(hostname, exit_code, status_text)
-
-    with warnings.catch_warnings():
-        # ignore the SSL related warning as this is internal
-        warnings.simplefilter("ignore")
-        _ = requests.get(passive_check_endpoint, verify=False, params={'token': token,
-                                                                       'cmd': 'submitcmd',
-                                                                       'command': submit_cmd})
+    if int(exit_code):
+        # send SNS notification here
+        print(status_text, exit_code)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--api-endpoint', type=str, help="Garage status API endpoint")
-    parser.add_argument('--submit-check-url', type=str, required=True, help="Nagios passive check url")
-    parser.add_argument('--token', type=str, required=True, help="Nagios passive check token")
-    parser.add_argument('--hostname', type=str, required=True, help="Nagios passive check url")
+    parser.add_argument('--api-endpoint', type=str, default='http://localhost:25678/garage/status',
+                        help="Garage status API endpoint")
     args = parser.parse_args()
 
-    submit_check(args.api_endpoint, args.submit_check_url, args.token, args.hostname)
+    submit_check(args.api_endpoint)
