2b1e620de84748c0ace7e7241bf031632066b947
Avi Oza <avioza@gmail.com>
2020-05-09 23:53:18 -0400

    sample script to send alert using sns script

---
+++ roles/copy-scripts/templates/send_nagios_alert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+export MSG=""
+
+# service
+if [ $# -gt 0 ]; then
+    MSG=$(cat <<END
+***** Nagios *****
+Notification Type: ${NAGIOS_NOTIFICATIONTYPE}
+State: ${NAGIOS_HOSTSTATE}
+Address: ${NAGIOS_HOSTADDRESS}
+Info: ${NAGIOS_SERVICEOUTPUT}
+Date/Time: ${NAGIOS_LONGDATETIME}
+END
+)
+
+else
+    MSG=$(cat <<END
+***** Nagios *****
+Notification Type: ${NAGIOS_NOTIFICATIONTYPE}
+State: ${NAGIOS_HOSTSTATE}
+Address: ${NAGIOS_HOSTADDRESS}
+Info: ${NAGIOS_HOSTOUTPUT}
+Date/Time: ${NAGIOS_LONGDATETIME}
+END
+)
+
+fi
+
+#AWS_DEFAULT_REGION={{ vault_aws_default_region }} AWS_ACCESS_KEY_ID={{ vault_aws_access_key_id }} AWS_SECRET_ACCESS_KEY={{ vault_aws_secret_access_key }} /opt/anaconda/bin/python {{ scripts_dir }}/send_sns_alert.py --sns-arn={{ vault_nagios_alert_sns_endpoint }}
