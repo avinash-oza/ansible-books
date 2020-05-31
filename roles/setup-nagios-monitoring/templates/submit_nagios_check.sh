627cde8b47607b45322f4e287b78917a53a1fa40
Avi Oza <avioza@gmail.com>
2020-05-31 09:52:22 -0400

    fix typo in cmd script for checks

--- roles/setup-nagios-monitoring/templates/submit_nagios_check.sh
+++ roles/setup-nagios-monitoring/templates/submit_nagios_check.sh
@@ -1,4 +1,4 @@
 # requires SERVICE_NAME and CHECK_CMD
-CMD_OUTPUT="$(/usr/lib/nagios/plugins/"$CHECK_CMD" "$CHECK_ARGS")"
+CMD_OUTPUT="$(/usr/lib/nagios/plugins/"$CHECK_CMD" $CHECK_ARGS)"
 
-/usr/local/nrdp/clients/send_nrdp.sh -u {{ vault_nagios_submit_passive_check_url }} -t {{ vault_nagios_passive_checks_api_key }} -H {{ nagios_host_name }} -s "$SERVICE_NAME" -S "$?" -o "$CMD_OUTPUT"
+{{ scripts_dir }}/send_nrdp.sh -u {{ vault_nagios_submit_passive_check_url }} -t {{ vault_nagios_passive_checks_api_key }} -H {{ nagios_host_name }} -s "$SERVICE_NAME" -S "$?" -o "$CMD_OUTPUT"
