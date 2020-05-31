b4bebe397c9e943fe45caa1fa9c9e2fcfab3692c
Avi Oza <avioza@gmail.com>
2020-05-31 09:15:02 -0400

    fix role for setting up monitoring

--- roles/copy-scripts/templates/submit_nagios_check.sh
+++
@@ -1,4 +0,0 @@
-# requires SERVICE_NAME and CHECK_CMD
-CMD_OUTPUT="$(/usr/lib/nagios/plugins/"$CHECK_CMD")"
-
-/usr/local/nrdp/clients/send_nrdp.sh -u <PATH_TO_NRDP> -t {{ vault_nagios_passive_checks_api_key }} -H {{ nagios_host_name }} -s "$SERVICE_NAME" -S "$?" -o "$CMD_OUTPUT"
