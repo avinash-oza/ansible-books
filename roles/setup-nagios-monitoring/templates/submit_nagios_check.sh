10e221b63ce8dfaa99bb78798debd8751209a264
Avi Oza <avioza@gmail.com>
2020-06-06 13:02:40 -0400

    update script to handle external checks

--- roles/setup-nagios-monitoring/templates/submit_nagios_check.sh
+++ roles/setup-nagios-monitoring/templates/submit_nagios_check.sh
@@ -1,4 +1,11 @@
-# requires SERVICE_NAME and CHECK_CMD
-CMD_OUTPUT="$(/usr/lib/nagios/plugins/"$CHECK_CMD" $CHECK_ARGS)"
+# requires SERVICE_NAME
+# specify CHECK_SCRIPT/CHECK_ARGS if an external script produces the output
+# otherwise CHECK_CMD/CHECK_ARGS should be nagios plugins
+
+# use CMD if defined else fall back to standard nagios plugins
+if [ ! -z ${CHECK_SCRIPT+x} ];
+then CMD_OUTPUT="$($CHECK_SCRIPT $CHECK_ARGS)";
+else CMD_OUTPUT="$(/usr/lib/nagios/plugins/"$CHECK_CMD" $CHECK_ARGS)";
+fi
 
 {{ scripts_dir }}/send_nrdp.sh -u {{ vault_nagios_submit_passive_check_url }} -t {{ vault_nagios_passive_checks_api_key }} -H {{ nagios_host_name }} -s "$SERVICE_NAME" -S "$?" -o "$CMD_OUTPUT"
