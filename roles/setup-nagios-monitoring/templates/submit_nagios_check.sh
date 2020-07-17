71c3a84bf79a6ff1c15f1b989c541cbfc8fbc17d
Avinash <avioza@gmail.com>
2020-07-17 07:49:26 -0400

    add nagios monitoring for garage, refactor variable to not be in the vault

--- roles/setup-nagios-monitoring/templates/submit_nagios_check.sh
+++ roles/setup-nagios-monitoring/templates/submit_nagios_check.sh
@@ -8,4 +8,4 @@ then CMD_OUTPUT="$($CHECK_SCRIPT $CHECK_ARGS)";
 else CMD_OUTPUT="$(/usr/lib/nagios/plugins/"$CHECK_CMD" $CHECK_ARGS)";
 fi
 
-{{ scripts_dir }}/send_nrdp.sh -u {{ vault_nagios_submit_passive_check_url }} -t {{ vault_nagios_passive_checks_api_key }} -H {{ nagios_host_name }} -s "$SERVICE_NAME" -S "$?" -o "$CMD_OUTPUT"
+{{ scripts_dir }}/send_nrdp.sh -u {{ nagios_submit_passive_check_url }} -t {{ vault_nagios_passive_checks_api_key }} -H {{ nagios_host_name }} -s "$SERVICE_NAME" -S "$?" -o "$CMD_OUTPUT"
