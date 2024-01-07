5922276660f6df4bcde050dfaa523a8529be24e7
Avi Oza <avioza@gmail.com>
2024-01-06 22:37:52 -0500

    add initial local setup for wireguard

--- roles/deploy-wireguard/templates/postup.sh
+++ roles/deploy-wireguard/templates/postup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 WIREGUARD_INTERFACE=wg0
 WIREGUARD_LAN=10.0.0.0/24
-MASQUERADE_INTERFACE=enp4s0
+MASQUERADE_INTERFACE={{ vault_wireguard_bridge_interface }}
 
 # Add a WIREGUARD_wg0 chain to the FORWARD chain
 CHAIN_NAME="WIREGUARD_$WIREGUARD_INTERFACE"
@@ -10,6 +10,7 @@ iptables -A FORWARD -j $CHAIN_NAME
 
 # Accept related or established traffic
 iptables -A $CHAIN_NAME -o $WIREGUARD_INTERFACE -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -o $MASQUERADE_INTERFACE -j MASQUERADE
 
 # Accept traffic from any Wireguard IP address connected to the Wireguard server
 iptables -A $CHAIN_NAME -s $WIREGUARD_LAN -i $WIREGUARD_INTERFACE -j ACCEPT
